a1cec31ba656bff1cf8c5f3e63e322b79e5cc04a
matthew goodman <mattsgood2@aol.com>
2016-03-07 14:18:36 +0000

    getting there
    will check if name in the list

--- names.py
+++ names.py
@@ -1,25 +1,35 @@
 class Asking:
-    my_list = []
+    my_list = [
+
+    'matthew',
+    'pim',
+    'jessica',
+    'leo',
+    'lillie'
+
+    ]
 
     def __init__(self):
-        self.names = input("Name please > ")
-        self.my_list.append(self.names)
 
         while True:
-            self.new_name = input("Next name please > ")
-            self.my_list.append(self.new_name)
-            if len(self.my_list) == 5:
-                print(self.my_list)
-                break
+            self.names = input("Name please > ")
+            if len(self.names) <= 1:
+                print("longer name")
+
+            if self.names in self.my_list:
+                print("Name Has Been Taken Alread Try Again")
+
+            elif self.names not in self.my_list:
+                self.my_list.append(self.names)
+
+                if len(self.my_list) == 10:
+                    print(self.my_list)
+                    break
+
+
 
     # add a name checking if name in my_list print info on them
     # add age checking
     # add info page to then check info on
-
-
-
-
-
-
 
 Asking()
