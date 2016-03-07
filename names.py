5583af932db4e4a49f352267221beee06bee2f39
matthew goodman <mattsgood2@aol.com>
2016-03-07 10:06:05 +0000

    tidy up
    clean up

--- names.py
+++ names.py
@@ -2,16 +2,19 @@ class Asking:
     my_list = []
 
     def __init__(self):
-        self.names = input("name please > ")
+        self.names = input("Name please > ")
         self.my_list.append(self.names)
 
         while True:
-            self.new_name = input("next name please > ")
+            self.new_name = input("Next name please > ")
             self.my_list.append(self.new_name)
             if len(self.my_list) == 5:
                 print(self.my_list)
                 break
 
+    # add a name checking if name in my_list print info on them
+    # add age checking
+    # add info page to then check info on
 
 
 
